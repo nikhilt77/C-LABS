15fcd73f6a134cd9f389ec1db50dabd0a561d81f
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2023-11-07 01:48:49 +0530

    Create BasicAbstractclass.java

---
+++ BasicAbstractclass.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Shape{
+	int numberofSides() {
+		return 0;
+	}
+}
+class Rectangle extends Shape{
+	int numberofSides() {
+	return 4;
+}
+}
+class Triangle extends Shape{
+	int numberofSides() {
+		return 3;
+	}
+}
+class Hexagon extends Shape{
+	int numberofSides() {
+		return 6;
+	}
+}
+
+public class Abstract {
+
+	public static void main(String[] args) {
+		Scanner p=new Scanner(System.in);
+		Rectangle r=new Rectangle();
+		Triangle t=new Triangle();
+		Hexagon h=new Hexagon();
+		System.out.println("Number of sides in Rectangle:"+r.numberofSides());
+		System.out.println("Number of sides in Triangle:"+t.numberofSides());
+		System.out.println("Number of sides in Hexagon:"+h.numberofSides());
+
+	}
+
+}
