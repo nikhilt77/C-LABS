27e1360cb26da89dfb7e925fe40ceee60c22db33
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2023-11-21 19:18:06 +0530

    Create BasicLinkedList.c

---
+++ BasicLinkedList.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node*next;
+};
+int main(){
+    struct node*first=malloc(sizeof(struct node));
+    first->data=40;
+    first->next=NULL;
+    struct node*second=malloc(sizeof(struct node));
+    second->data=50;
+    second->next=NULL;
+    first->next=second;
+    struct node*third=malloc(sizeof(struct node));
+    third->data=60;
+    third->next=NULL;
+    second->next=third;
+    struct node*current=first;
+    while(current!=NULL){
+        printf("%d->",current->data);
+        current=current->next;
+    }
+    return 0;
+}
