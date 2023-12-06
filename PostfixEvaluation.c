3024a5c2195e72649a6d763b6ba16ead124a8e7f
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2023-12-06 08:25:11 +0530

    Update PostfixEvaluation.c

--- PostfixEvaluation.c
+++ PostfixEvaluation.c
@@ -1,63 +1,60 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<stdbool.h>
-#define M_size 100
-int S[100],top=-1;
-void push(int x){
-    if(top!=M_size-1){
-        top=top+1;
-        S[top]=x;
-    }
+#include<stdlib.h>
+#include<math.h>
+#define M_Size 100
+int stack[M_Size];
+int top=-1;
+void push(int data){
+    top++;
+    stack[top]=data;
 }
-String pop(){
-    if(top!=-1){
-        top=top-1;
-        return S[top+1];
-    }
+int pop(){
+    top--;
+    return stack[top+1];
 }
-bool isOperator(char ch){
-    if(ch=='+'||ch=='-'||ch=='*'||ch=='^'||ch=='/'){
-        return true;
+bool isOperator(char c){
+    switch(c){
+        case '*':
+        case '-':
+        case '+':
+        case '/':
+        case '^':return 1;
+        default:return 0;
+    }
+    
+}
+int convert(char c){
+    return c-48;
+}
+void perform(int b,char c,int a){
+    int var;
+    switch(c){
+
+        case '+':var=a+b;break;
+        case '-':var=a-b;break;
+        case '*':var=a*b;break;
+        case '/':var=a/b;break;
+        case '^':var=pow(a,b);break;
+    }
+    push(var);
+}
+void pass(char c){
+    if(isOperator(c)){
+        perform(pop(),c,pop());
     }
     else{
-        return false;
+        push(convert(c));
     }
 }
 void main(){
-    String A[30];
-    float r;
-    printf("Enter Expression:");
-    for(int i=0;i<30;i++){
-        scanf("%s",&A[i]);
+    char postfix[M_Size];
+    printf("Enter postfix expression:");
+    scanf("%s",&postfix);
+    for(int i=0;infix[i]!='\0';i++){
+        pass(infix[i]);
     }
-    int i=0;
-    while(i!=A.length()){
-        if(A[i]>='0'&& A[i]<'9'){
-            int n=A[i];
-            push(n);
-        }
-        else if(isOperator(A[i])){
-            String o2=pop();
-            String o1=pop();
-            if(A[i]=='*'){
-                r=o1*o2;
-            }
-            else if(A[i]=='/'){
-                r=o1/o2;
-            }
-            else if(A[i]=='+'){
-                r=o1+o2;
-            }
-            else if(A[i]=='-'){
-                r=o1-o2;
-            }
-            else{
-                r=o1^o2;
-            }
-        }
-        push(r);
-        }
-        i++;
-    
-    printf(pop());
-    }
+    printf("Result:\n");
+    printf("%d",stack[top]);
+}
+
