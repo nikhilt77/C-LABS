1aa4eed290881cf56509456f45817ea6806846cc
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2024-02-10 17:17:58 +0530

    Update PriorityQueue.c

--- S-3/PriorityQueue.c
+++ S-3/PriorityQueue.c
@@ -1,141 +1,68 @@
-
-#include <stdio.h>
-
-#include <stdlib.h>
-#define MAX 50
-
-int prique[MAX];
-int front=-1, rear=-1;
- 
-void main()
-{
-    int n, ch;
- 
-    printf("\n1 - Insert an element into queue");
-    printf("\n2 - Delete an element from queue");
-    printf("\n3 - Display queue elements");
-    printf("\n4 - Exit");
- 
-    //create();
- 
-    while (1)
-    {
-        printf("\nEnter your choice : ");    
-        scanf("%d", &ch);
- 
-        switch (ch)
-        {
-        case 1: 
-            printf("\nEnter value to be inserted : ");
-            scanf("%d",&n);
-            insert(n);
-            break;
-        case 2:
-            printf("\nEnter value to delete : ");
-            scanf("%d",&n);
-            delete(n);
-            break;
-        case 3: 
-            display();
-            break;
-        case 4: 
-            exit(0);
-        default: 
-            printf("\nChoice is incorrect, Enter a correct choice");
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+struct node{
+    int data;
+    int priority;
+    struct node*next;
+}*head=NULL,*temp=NULL,*newnode=NULL,*current;
+struct node*create(int data,int priority){
+    newnode=malloc(sizeof(struct node));
+    newnode->next=NULL;
+    newnode->data=data;
+    newnode->priority=priority;
+    return newnode;
+}
+void enqueue(struct node**head,int data,int priority){
+    current=*head;
+    newnode=create(data,priority);
+    if((*head)==NULL){
+        newnode->next=*head;
+        *head=newnode;
+    }
+    else if((*head)->priority>priority){
+        newnode->next=*head;
+        *head=newnode;
+    }
+    else{
+        while(current->next!=NULL&&current->next->priority<priority){
+                current=current->next;
+        }
+        newnode->next=current->next;
+        current->next=newnode;
+    }
+}
+void deque(struct node**head){
+    temp=*head;
+    *head=(*head)->next;
+    free(temp);
+}
+void display(struct node*head){
+    current=head;
+    while(current->next!=NULL){
+        printf("%d->",current->data);
+        current=current->next;
+    }
+   printf("->%d\n",current->data);
+}
+int main(){
+    int ch,n,p;
+    while(true){
+        printf("1.Enqueue\n2.Dequeue\n3.Display\n");
+        printf("Enter choice:");
+        scanf("%d",&ch);
+        switch(ch){
+            case 1:printf("Enter element and priority:");
+                    scanf("%d",&n);
+                    scanf("%d",&p);
+                    enqueue(&head,n,p);
+                    break;
+            case 2:deque(&head);
+                    break;
+            case 3:display(head);
+                    break;
+            default:printf("Invalid choice");        
         }
     }
-}
- 
- //Function to create an empty priority queue 
-void create()
-{
-    front = rear = -1;
-}
- 
-
-void insert(int data)
-{
-    if (rear >= MAX - 1)
-    {
-        printf("\nQueue overflow no more elements can be inserted");
-        return;
-    }
-    if ((front == -1) && (rear == -1))
-    {
-        front++;
-        rear++;
-        prique[rear] = data;
-        return;
-    }    
-    else
-        check(data);
-    rear++;
-}
- 
-/* Function to check priority and place element */
-void check(int data)
-{
-    int i,j;
- 
-    for (i = 0; i <= rear; i++)
-    {
-        if (data >= prique[i])
-        {
-            for (j = rear + 1; j > i; j--)
-            {
-                prique[j] = prique[j - 1];
-            }
-            prique[i] = data;
-            return;
-        }
-    }
-    prique[i] = data;
-}
- 
-/* Function to delete an element from queue */
-void delete(int data)
-{
-    int i;
- 
-    if ((front==-1) && (rear==-1))
-    {
-        printf("\nQueue is empty no elements to delete");
-        return;
-    }
- 
-    for (i = 0; i <= rear; i++)
-    {
-        if (data == prique[i])
-        {
-            for (; i < rear; i++)
-            {
-                prique[i] = prique[i + 1];
-            }
- 
-       
-        rear--;
- 
-        if (rear == -1) 
-            front = -1;
-        return;
-        }
-    }
-    printf("\n%d not found in queue to delete", data);
-}
- 
-/* Function to display queue elements */
-void display()
-{
-    if ((front == -1) && (rear == -1))
-    {
-        printf("\nQueue is empty");
-        return;
-    }
- 
-    for (; front <= rear; front++)
-    {
-        printf(" %d ", prique[front]);
-    }
- 
-    front = 0;
+    return 0;
 }
