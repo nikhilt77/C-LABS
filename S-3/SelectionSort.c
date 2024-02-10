5fc9693230fd8cc53cf87621c4d8ee44df89d547
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2024-02-10 23:51:25 +0530

    Update SelectionSort.c

--- S-3/SelectionSort.c
+++ S-3/SelectionSort.c
@@ -1,25 +1,31 @@
 // Selection sort
-#include <stdio.h>
+#include<stdio.h>
 
-void main() {
-    int n,temp;
-    printf("Enter Array size: ");
+void main(){
+    int n,min,temp;
+    
+    printf("Enter elements:");
     scanf("%d",&n);
     int A[n];
-    printf("Enter Array: ");
+    printf("Enter elements of A:");
     for(int i=0;i<n;i++){
         scanf("%d",&A[i]);
     }
     for(int i=0;i<n-1;i++){
+        min=i;
         for(int j=i+1;j<n;j++){
-            if(A[i]>A[j]){
-                temp=A[i];
-                A[i]=A[j];
-                A[j]=temp;
+            if(A[min]>A[j]){
+                min=j;
             }
         }
+        if(min!=i){
+            int temp;
+            temp=A[i];
+            A[i]=A[min];
+            A[min]=temp;
+        }
     }
-    printf("Sorted Array: ");
+    printf("Sorted array:");
     for(int i=0;i<n;i++){
         printf("%d\t",A[i]);
     }
