dc84bd7651698b742b43925d1f9599dff32d97cf
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2024-08-07 23:20:18 +0530

    Create alphaTriangle.cpp

---
+++ alphaTriangle.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void alphaTriangle(int n) {
+    for(int i = 0; i < n; i++) {
+        char ch = 'A' + n - 1;
+        for(int j = 0; j <= i; j++) {
+            cout << ch << " ";
+            ch--;
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    alphaTriangle(n);
+    return 0;
+}
